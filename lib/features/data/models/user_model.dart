4413a5dd57812c0a6e7f3090932279a450c867f1
Jorge A. Mogotocoro F <jorgemogotocoro05@outlook.es>
2022-11-20 17:29:22 -0500

    project cleanup

--- lib/features/data/models/user_model.dart
+++ lib/features/data/models/user_model.dart
@@ -23,41 +23,6 @@ class UserModel extends UserEntity {
           company: company,
         );
 
-  factory UserModel.cast(UserEntity? e) {
-    return UserModel(
-      id: e?.id,
-      name: e?.name,
-      username: e?.username,
-      email: e?.email,
-      address: AddressModel.cast(e?.address),
-      phone: e?.phone,
-      website: e?.website,
-      company: CompanyModel.cast(e?.company),
-    );
-  }
-
-  UserModel copyWithModel({
-    int? id,
-    String? name,
-    String? username,
-    String? email,
-    AddressModel? address,
-    String? phone,
-    String? website,
-    CompanyModel? company,
-  }) {
-    return UserModel(
-      id: id ?? this.id,
-      name: name ?? this.name,
-      username: username ?? this.username,
-      email: email ?? this.email,
-      address: address ?? AddressModel.cast(this.address),
-      phone: phone ?? this.phone,
-      website: website ?? this.website,
-      company: company ?? CompanyModel.cast(this.company),
-    );
-  }
-
   factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
         id: json["id"],
         name: json["name"],
@@ -78,9 +43,4 @@ class UserModel extends UserEntity {
       phone: query["phone"],
     );
   }
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-      };
 }
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -4,20 +4,12 @@ import 'package:ceiba_technical_test/features/app/blocs/posts_list_bloc/posts_li
 import 'package:ceiba_technical_test/features/data/datasource/user_local_data_source.dart';
 import 'package:ceiba_technical_test/features/domain/usecases/get_publications_list_use_case.dart';
 import 'package:ceiba_technical_test/features/domain/usecases/get_user_list_use_case_usecase.dart';
-import 'package:ceiba_technical_test/features/domain/usecases/register_user_usecase.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ceiba_technical_test/features/app/blocs/splash_bloc/splash_bloc.dart';
-import 'package:ceiba_technical_test/features/data/datasource/auth_local_data_source.dart';
-import 'package:ceiba_technical_test/features/data/datasource/auth_remote_data_source.dart';
 import 'package:ceiba_technical_test/features/data/datasource/user_remote_data_source.dart';
-import 'package:ceiba_technical_test/features/data/repositories/auth_repository_impl.dart';
 import 'package:ceiba_technical_test/features/data/repositories/user_repository_impl.dart';
-import 'package:ceiba_technical_test/features/domain/repositories/auth_repository.dart';
 import 'package:ceiba_technical_test/features/domain/repositories/user_repository.dart';
-import 'package:ceiba_technical_test/features/domain/usecases/check_authenticated_use_case.dart';
-import 'package:ceiba_technical_test/features/domain/usecases/get_current_user_usecase.dart';
-import 'package:ceiba_technical_test/features/domain/usecases/reject_user_confirmation_usecase.dart';
 
 final sl = GetIt.instance;
 
@@ -26,8 +18,7 @@ Future<void> init() async {
    * Blocs
    */
 
-  sl.registerFactory(
-      () => SplashBloc(checkAuthenticated: sl(), rejectUserConfirmation: sl()));
+  sl.registerFactory(() => SplashBloc());
   sl.registerFactory(() => HomeBloc(getUserUseCase: sl()));
   sl.registerFactory(() => PostsListBloc(getPostsListUseCase: sl()));
 
@@ -35,10 +26,6 @@ Future<void> init() async {
    * Use Cases
    */
 
-  sl.registerLazySingleton(() => CheckAuthenticatedUseCase(sl()));
-  sl.registerLazySingleton(() => RejectUserConfirmationUseCase(sl()));
-  sl.registerLazySingleton(() => GetCurrentUserUsecase(sl()));
-  sl.registerLazySingleton(() => SetUserUseCase(sl()));
   sl.registerLazySingleton(() => GetUserListUseCase(sl()));
   sl.registerLazySingleton(() => GetPostsListUseCase(sl()));
 
@@ -48,8 +35,6 @@ Future<void> init() async {
 
   sl.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(
       userRemoteDataSource: sl(), userLocalDataSource: sl()));
-  sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl(
-      authRemoteDataSource: sl(), authLocalDataSource: sl()));
 
   /**
    * Data Sources
@@ -58,10 +43,6 @@ Future<void> init() async {
       () => UserRemoteDataSourceImpl());
   sl.registerLazySingleton<UserLocalDataSource>(
       () => UserLocalDataSourceImpl(databaseHelper: sl()));
-  sl.registerLazySingleton<AuthRemoteDataSource>(
-      () => AuthRemoteDataSourceImpl());
-  sl.registerLazySingleton<AuthLocalDataSource>(
-      () => AuthLocalDataSourceImpl(sharedPreferences: sl()));
 
   /**
    * Database 
